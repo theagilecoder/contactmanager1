9f5b13584b0f6e497ed3fc2215b6ae0cb9ced401
Pushkar Singh <theagilecoder@gmail.com>
2018-10-28 22:00:34 +0530

    Put Contact in Contacts Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,16 +10,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe@gmail.com"
-            phone="555-555-5555"
-          />
-          <Contact
-            name="Karen Doe"
-            email="jdoe@gmail.com"
-            phone="555-555-5555"
-          />
+          <Contacts />
         </div>
       </div>
     );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,34 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export default class Contacts extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {this.state.showContactInfo && (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
 }
 
 Contacts.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+  contact: PropTypes.object.isRequired
 };
