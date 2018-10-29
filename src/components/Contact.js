f508560bd6244b488858c18fdd8520d8b089f069
Pushkar Singh <theagilecoder@gmail.com>
2018-10-29 23:12:25 +0530

    Implement delete contact using Context-API

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../context";
 
-export default class Contacts extends Component {
+export default class Contact extends Component {
   state = {
     showContactInfo: false
   };
@@ -12,38 +13,45 @@ export default class Contacts extends Component {
     });
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{" "}
-          <i
-            onClick={this.onShowClick}
-            className="fas fa-sort-down"
-            style={{ cursor: "pointer" }}
-          />
-          <i
-            onClick={this.onDeleteClick}
-            className="fas fa-times"
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-          />
-        </h4>
-        {this.state.showContactInfo && (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        )}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{" "}
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                />
+              </h4>
+              {this.state.showContactInfo && (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              )}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
-Contacts.propTypes = {
+Contact.propTypes = {
   contact: PropTypes.object.isRequired
 };
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,50 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 3,
-        name: "Jack Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={() => this.deleteContact(contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
