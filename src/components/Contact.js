d194040cda4bbe124839ee85ff00fbff374e57c8
Pushkar Singh <theagilecoder@gmail.com>
2018-10-29 21:47:51 +0530

    Implement delete contact using prop-drilling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,12 +12,26 @@ export default class Contacts extends Component {
     });
   };
 
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          {name}{" "}
+          <i
+            onClick={this.onShowClick}
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            onClick={this.onDeleteClick}
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+          />
         </h4>
         {this.state.showContactInfo && (
           <ul className="list-group">
