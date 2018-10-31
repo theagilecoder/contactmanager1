bb9470892c07fd60af59f66049cbe4fda4ed8f73
Pushkar Singh <theagilecoder@gmail.com>
2018-10-31 22:10:37 +0530

    Updates

---
+++ precache-manifest.0b108a451a27cfebe9afdee7911209d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5468eef6b3679363afe4",
+    "url": "/contactmanager1/static/js/runtime~main.5468eef6.js"
+  },
+  {
+    "revision": "a5246e9d9573fcfb7c49",
+    "url": "/contactmanager1/static/js/main.a5246e9d.chunk.js"
+  },
+  {
+    "revision": "762e2d1b5bd77ed69326",
+    "url": "/contactmanager1/static/js/1.762e2d1b.chunk.js"
+  },
+  {
+    "revision": "a5246e9d9573fcfb7c49",
+    "url": "/contactmanager1/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "762e2d1b5bd77ed69326",
+    "url": "/contactmanager1/static/css/1.c9045b4b.chunk.css"
+  },
+  {
+    "revision": "0046c7f745e932681ad66bac3a4ed2d5",
+    "url": "/contactmanager1/index.html"
+  }
+];
