1dd0cbec6b77d7793fd00c81509474e23188c206
Pushkar Singh <theagilecoder@gmail.com>
2018-10-31 12:38:00 +0530

    Add 404 page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -20,6 +21,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
