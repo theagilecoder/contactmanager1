a81b183a29ab9caff7a5bb37962f9fb27f6bc085
Pushkar Singh <theagilecoder@gmail.com>
2018-10-31 21:49:20 +0530

    Implement Edit Contact Functionality

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Consumer } from "../../context";
 import axios from "axios";
@@ -43,6 +44,17 @@ export default class Contact extends Component {
                   className="fas fa-times"
                   style={{ cursor: "pointer", float: "right", color: "red" }}
                 />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: "pointer",
+                      float: "right",
+                      color: "black",
+                      marginRight: "1rem"
+                    }}
+                  />
+                </Link>
               </h4>
               {this.state.showContactInfo && (
                 <ul className="list-group">
